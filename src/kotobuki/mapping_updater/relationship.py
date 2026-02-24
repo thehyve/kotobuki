2daa833cbb3eea1354da46ce579f0c4292467209
Spayralbe <stefan@thehyve.nl>
2026-02-24 17:11:11 +0100

    Add to_map_path_data for NewMap

--- src/kotobuki/mapping_updater/relationship.py
+++ src/kotobuki/mapping_updater/relationship.py
@@ -33,9 +33,9 @@ class MapLink:
     via: Relationship | None = None
 
     def __str__(self) -> str:
-        s = f"[{self.concept.concept_id}] {self.concept.concept_name}"
+        s = f"{self.concept.concept_id} {self.concept.concept_name}"
         if self.via is not None:
-            s = f"➡️ <{self.via.value}> {s}"
+            s = f"[{self.via.value}] {s}"
         return s
 
 
@@ -50,6 +50,26 @@ class NewMap:
         if self.value_as_concept:
             s = f"{s}\nMaps to value: {','.join(str(c.concept_id) for c in self.value_as_concept)}"
         return s
+
+    def to_map_path_data(self) -> dict[str, dict[str, list[str]]]:
+        """Convert to dict suitable for writing to mapping path file."""
+        if not self.map_path:
+            return {}
+        # we only want to show the intermediate steps in the mapping
+        # path, so we ignore the source concept and the final target
+        map_path = [str(ml) for ml in self.map_path[1:-1]]
+        map_properties = {
+            "map_path": map_path,
+            "maps_to": [f"{c.concept_id} {c.concept_name}" for c in self.concepts],
+            "maps_to_value": [f"{c.concept_id} {c.concept_name}" for c in self.value_as_concept],
+        }
+        # Remove keys that point to empty lists
+        map_properties = {k: v for k, v in map_properties.items() if v}
+
+        original_concept = self.map_path[0].concept
+        return {
+            f"{original_concept.concept_id} {original_concept.concept_name}": map_properties,
+        }
 
     def render_map_path(self) -> str:
         return " ".join([str(ml) for ml in self.map_path])
--- tests/python/mapping_updater/test_mapping_paths.py
+++ tests/python/mapping_updater/test_mapping_paths.py
@@ -1,22 +1,69 @@
-from pathlib import Path
+from omop_cdm.regular.cdm54 import Concept
 
-from sqlalchemy import Engine
+from kotobuki.mapping_updater.relationship import MapLink, NewMap, Relationship
 
-from kotobuki import update_usagi_file
-from tests.python.mapping_updater.conftest import USAGI_STCM_FILE, write_tmp_usagi_file
+TARGET_CONCEPT1 = Concept(concept_id=3, concept_name="The golden standard")
+TARGET_CONCEPT2 = Concept(concept_id=30, concept_name="The other golden standard")
+SOURCE_CONCEPT1 = Concept(concept_id=2, concept_name="The golden non-standard")
+HOMONYM_CONCEPT1 = Concept(concept_id=99, concept_name="THE GOLDEN NON-STANDARD")
 
 
-def test_write_map_paths(tmp_path: Path, pg_db_engine: Engine):
-    tmp_usagi_file = write_tmp_usagi_file(tmp_path, USAGI_STCM_FILE)
-    update_usagi_file(pg_db_engine, "vocab", tmp_usagi_file, write_map_paths=True)
-    map_paths_file = next(tmp_path.glob("*.txt"))
-    with map_paths_file.open("r", encoding="utf8") as f:
-        lines = {line.strip() for line in f}
-    expected_lines = {
-        "[2] x depricated2 ➡️ <Maps to> [3] x standard",
-        "[1] x depricated1 ➡️ <Concept replaced by> [2] x depricated2 ➡️ <Maps to> [3] x standard",
-        "[7] History of halitosis ➡️ <Maps to> [8] History of",
-        "[10] one_to_two_map ➡️ <Maps to> [11] 1/2 ➡️ <Maps to> [12] 2/2",
-        "[13] dep_concept_two_values ➡️ <Maps to> [14] superstandard",
+def test_simple_mapping_path():
+    map_links = [
+        MapLink(concept=SOURCE_CONCEPT1),
+        MapLink(concept=TARGET_CONCEPT1, via=Relationship.MAPS_TO),
+    ]
+    new_map = NewMap(concepts=[TARGET_CONCEPT1], map_path=map_links)
+    map_path_data = new_map.to_map_path_data()
+    assert map_path_data == {
+        "2 The golden non-standard": {
+            "maps_to": ["3 The golden standard"],
+        }
     }
-    assert expected_lines == lines
+
+
+def test_via_homonym_mapping_path():
+    map_links = [
+        MapLink(concept=SOURCE_CONCEPT1),
+        MapLink(concept=HOMONYM_CONCEPT1, via=Relationship.HOMONYM),
+        MapLink(concept=TARGET_CONCEPT1, via=Relationship.MAPS_TO),
+    ]
+    new_map = NewMap(concepts=[TARGET_CONCEPT1], map_path=map_links)
+    map_path_data = new_map.to_map_path_data()
+    assert map_path_data == {
+        "2 The golden non-standard": {
+            "map_path": ["[Homonym] 99 THE GOLDEN NON-STANDARD"],
+            "maps_to": ["3 The golden standard"],
+        }
+    }
+
+
+# def test_one_to_two_mapping_path():
+#     map_links= [
+#         MapLink(concept=SOURCE_CONCEPT1),
+#         MapLink(concept=TARGET_CONCEPT1, via=Relationship.MAPS_TO),
+#     ]
+#     new_map = NewMap(concepts=[TARGET_CONCEPT1], map_path=map_links)
+#     map_path_data = new_map.to_map_path_data()
+#     assert map_path_data == {
+#         "2 The golden non-standard": {
+#             "map_path": ["[Homonym] 99 THE GOLDEN NON-STANDARD"],
+#             "maps_to": ["3 The golden standard"],
+#         }
+#     }
+
+#
+# def test_write_map_paths(tmp_path: Path, pg_db_engine: Engine):
+#     tmp_usagi_file = write_tmp_usagi_file(tmp_path, USAGI_STCM_FILE)
+#     update_usagi_file(pg_db_engine, "vocab", tmp_usagi_file, write_map_paths=True)
+#     map_paths_file = next(tmp_path.glob("*.txt"))
+#     with map_paths_file.open("r", encoding="utf8") as f:
+#         lines = {line.strip() for line in f}
+#     expected_lines = {
+#         "[2] x depricated2 ➡️ <Maps to> [3] x standard",
+#         "[1] x depricated1 ➡️ <Concept replaced by> [2] x depricated2 ➡️ <Maps to> [3] x standard",
+#         "[7] History of halitosis ➡️ <Maps to> [8] History of",
+#         "[10] one_to_two_map ➡️ <Maps to> [11] 1/2 ➡️ <Maps to> [12] 2/2",
+#         "[13] dep_concept_two_values ➡️ <Maps to> [14] superstandard",
+#     }
+#     assert expected_lines == lines
